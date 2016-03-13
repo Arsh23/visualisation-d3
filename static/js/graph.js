f8b487b625257d84dd2d794fb762cf11e1ab15d6
Arsh23 <arsh.bbps@gmail.com>
2016-03-13 17:35:51 +0530

    created initialize function

--- static/js/graph.js
+++ static/js/graph.js
@@ -4,7 +4,8 @@ d3.json('data',
 
         var updatedata = function() {
             var circle = d3.select(this);
-            circle.transition().duration(150)
+            circle.transition()
+                .duration(150)
                 .attr("r", 6);
             var name = d3.select(this).attr('id');
             $('.pulsarname').html(name)
@@ -35,19 +36,19 @@ d3.json('data',
             $('.binary').html('')
         }
 
+        // arrays for calculating domain
         var periods = [];
+        var periods_der = [];
         for (i = 0; i < data.length; i++) {
             periods.push(data[i]['Period'])
         }
-
-        var periods_der = [];
         for (i = 0; i < data.length; i++) {
             periods_der.push(Math.log10(data[i]['Period Derivative']))
         }
 
+        //size and margin
         var basewidth = $('.graph').width()
         var baseheight = $('.graph').height()
-
         var margin = {
                 top: 10,
                 right: 10,
@@ -57,7 +58,7 @@ d3.json('data',
             width = basewidth - margin.left - margin.right,
             height = baseheight - margin.top - margin.bottom;
 
-        // x related values
+        // x related values init
         var xscale = d3.scale.log()
             .domain([
                 Math.round(Math.floor(d3.min(periods) * 10000)) / 10000,
@@ -74,7 +75,7 @@ d3.json('data',
             .outerTickSize(0)
             .tickPadding(5);
 
-        // y related values
+        // y related values init
         var yscale = d3.scale.linear()
             .domain([
                 Math.floor(d3.min(periods_der)),
@@ -82,54 +83,69 @@ d3.json('data',
             ])
             .range([height - 0, 0]),
             yAxis = d3.svg.axis()
-            .scale(yscale)
-            .ticks(20)
-            .orient("left")
-            .innerTickSize(-width)
-            .outerTickSize(0)
-            .tickPadding(5);
+                .scale(yscale)
+                .ticks(20)
+                .orient("left")
+                .innerTickSize(-width)
+                .outerTickSize(0)
+                .tickPadding(5);
 
-
+        //zoom function and update
         var zoom = d3.behavior.zoom()
             .x(xscale)
             .y(yscale)
-            // .scaleExtent([1, 10])
-            .on("zoom", zoomed);
+            .on("zoom", function() {
+                g_xaxis.call(xAxis);
+                g_yaxis.call(yAxis);
+                console.log('zoom')
+                updategraph()
+            });
 
-        function zoomed() {
-            g_xaxis.call(xAxis);
-            g_yaxis.call(yAxis);
-            console.log('zoom')
-            updategraph()
+
+        function initializegraph() {
+
+            // arrays for calculating domain
+            for (i = 0; i < data.length; i++) {
+                periods.push(data[i]['Period'])
+            }
+            for (i = 0; i < data.length; i++) {
+                periods_der.push(Math.log10(data[i]['Period Derivative']))
+            }
+
+            // x related values
+            xscale
+                .domain([
+                    Math.round(Math.floor(d3.min(periods) * 10000)) / 10000,
+                    Math.round(Math.ceil(d3.max(periods) * 10000)) / 10000
+                ])
+                .range([0, width]);
+            xAxis
+                .scale(xscale)
+                .ticks(5, function(d) {
+                    return d.toExponential(2).replace('e', 'x10^')
+                })
+                .orient("bottom")
+                .innerTickSize(-height)
+                .outerTickSize(0)
+                .tickPadding(5);
+
+            // y related values
+            yscale
+                .domain([
+                    Math.floor(d3.min(periods_der)),
+                    Math.ceil(d3.max(periods_der))
+                ])
+                .range([height - 0, 0]);
+            yAxis
+                .scale(yscale)
+                .ticks(20)
+                .orient("left")
+                .innerTickSize(-width)
+                .outerTickSize(0)
+                .tickPadding(5);
+
+
         }
-
-        // main canvas
-        var canvas = d3.select('.graph')
-            .append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .call(zoom)
-            .append("g")
-            .attr("class", "points")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-
-        // y axis
-        var g_yaxis = canvas.append("g")
-            .attr("class", "axis axis--y")
-            .attr("position", "fixed")
-        g_yaxis.call(yAxis);
-
-        // x axis
-        var g_xaxis = canvas.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + (height) + ")")
-        g_xaxis.call(xAxis);
-
-        // plot points
-        canvas
-            .append("g")
-            .attr("class", "points")
 
         function updategraph() {
             var circle =
@@ -181,5 +197,35 @@ d3.json('data',
                     return yscale(Math.log10(d['Period Derivative']));
                 })
         }
+
+        //start plotting graph
+        // main canvas
+        var canvas = d3.select('.graph')
+            .append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .call(zoom)
+            .append("g")
+            .attr("class", "points")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+        // y axis
+        var g_yaxis = canvas.append("g")
+            .attr("class", "axis axis--y")
+            .attr("position", "fixed")
+        g_yaxis.call(yAxis);
+
+        // x axis
+        var g_xaxis = canvas.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + (height) + ")")
+        g_xaxis.call(xAxis);
+
+        // plot points
+        canvas
+            .append("g")
+            .attr("class", "points")
+
+        initializegraph()
         updategraph()
     });
